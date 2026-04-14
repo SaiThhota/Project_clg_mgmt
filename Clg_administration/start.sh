ab322d8ed1174036e1619587caa14b7d92fab231
saiteja <thotasaitej239@gmail.com>
2026-04-14 20:30:02 +0530

    updated start.sh

--- Clg_administration/start.sh
+++ Clg_administration/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn Clg_administration.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn CLG_ADMINISTRATION.wsgi:application --bind 0.0.0.0:$PORT
 
