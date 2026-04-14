edd69dbdfd39a3373297b3e59a2dd2da2aa810dd
saiteja <thotasaitej239@gmail.com>
2026-04-14 19:34:25 +0530

    Fix wsgi path

--- Clg_administration/start.sh
+++ Clg_administration/start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn Clg_administration.Clg_administration.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn Clg_administration.wsgi:application --bind 0.0.0.0:$PORT
+
